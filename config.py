8a72d8ec0cbd33e913a6338ef3e47bb4283af0dd
CoolinPack <zalyalievarsen7@gmail.com>
2026-08-18 14:22:46 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 class Config:
     # Telegram Bot
     BOT_TOKEN = os.getenv('BOT_TOKEN')
     ADMIN_CHAT_ID = os.getenv('ADMIN_CHAT_ID')
-    
+    GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')  # группа "Заявки на доставку"
+
     # Database
     DATABASE_PATH = os.getenv('DATABASE_PATH', './terassa.db')
-    
+
     # Static files
     IMAGES_FOLDER = 'static/images'
     UPLOAD_FOLDER = 'uploads'
